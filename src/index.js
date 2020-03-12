673c758880b3332b394b655e9466ce81e9df3c8b
Elena <helena_shevts@mail.ru>
2020-03-12 11:08:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
+      openBrackets.push(str[i]);
+    } else if (str[i] === ')' || str[i] === ']' || str[i] === '}') {
+      openBrackets.pop();
+    }
+  }
+  if (openBrackets.length === 0) {
+    return true;
+  } else return false;
 }
